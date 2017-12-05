028265f96b69085c87d9334903651ec2f377937a
Timothy Salazar <timothy.salazar@colorado.edu>
2017-12-05 16:50:28 -0700

    cleanup - removed jupyter notebook checkpoints

--- jupyspark.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PYSPARK_DRIVER_PYTHON=jupyter
-export PYSPARK_DRIVER_PYTHON_OPTS="notebook --NotebookApp.open_browser=True --NotebookApp.ip='localhost' --NotebookApp.port=8888"
-
